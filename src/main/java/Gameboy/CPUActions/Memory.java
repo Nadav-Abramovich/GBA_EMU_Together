eaaeeba22219a9f616390a309fa1b02f44168b31
Nadav <nadav.abramovich@gmail.com>
2021-03-05 08:45:52 +0200

    Used Map for declaring the SUPPORTED_ACTIONS Dictionary with entries so we may declare the functions outside the SUPPORTED_ACTIONS scope

--- src/main/java/Gameboy/CPUActions/Memory.java
+++ src/main/java/Gameboy/CPUActions/Memory.java
@@ -8,46 +8,47 @@ import java.util.Map;
 public class Memory extends CPUActions {
     private final CPU cpu_reference;
 
-    private final Map<Character, Runnable> SUPPORTED_ACTIONS = new HashMap<>() {
-        {
-            put((char) 0x21, this::ld_hl_d16);
-            put((char) 0x22, this::ld_hl_plus_a);
-            put((char) 0x32, this::ld_hl_minus_a);
-        }
-
-        private void ld_hl_d16() {
-            System.out.println("_ld_hl_d16");
-            int lowerByte = cpu_reference.memory[cpu_reference.PC + 1] & 255;
-            int upperByte = cpu_reference.memory[cpu_reference.PC + 2] & 255;
-            cpu_reference.HL = (char) (upperByte << 8 | lowerByte);
-            cpu_reference.PC += 3;
-        }
-
-        private void ld_hl_plus_a() {
-            System.out.println("_ld_hl_plus_a");
-
-            byte A = (byte) (cpu_reference.AF >> 8);
-
-            cpu_reference.memory[cpu_reference.HL] = A;
-            cpu_reference.HL++;
-            cpu_reference.PC++;
-        }
-
-        private void ld_hl_minus_a() {
-            System.out.println("_ld_hl_minus_a");
-
-            byte A = (byte) (cpu_reference.AF >> 8);
-
-            cpu_reference.memory[cpu_reference.HL] = A;
-            cpu_reference.HL--;
-            cpu_reference.PC++;
-        }
-    };
+    private final Map<Character, Runnable> SUPPORTED_ACTIONS = Map.ofEntries(
+            Map.entry((char) 0x21, this::ld_hl_d16),
+            Map.entry((char) 0x22, this::ld_hl_plus_a),
+            Map.entry((char) 0x32, this::ld_hl_minus_a)
+    );
 
     // NOTE: This constructor is used by a dynamic factory and therefore intellij doesn't recognize its usage
     public Memory(CPU cpu) {
         cpu_reference = cpu;
     }
 
-    public Map<Character, Runnable> get_supported_actions() { return SUPPORTED_ACTIONS; }
+    public Map<Character, Runnable> get_supported_actions() {
+        return SUPPORTED_ACTIONS;
+    }
+
+    private void ld_hl_d16() {
+        System.out.println("_ld_hl_d16");
+        int lowerByte = cpu_reference.memory[cpu_reference.PC + 1] & 255;
+        int upperByte = cpu_reference.memory[cpu_reference.PC + 2] & 255;
+        cpu_reference.HL = (char) (upperByte << 8 | lowerByte);
+        cpu_reference.PC += 3;
+    }
+
+    private void ld_hl_plus_a() {
+        System.out.println("_ld_hl_plus_a");
+
+        byte A = (byte) (cpu_reference.AF >> 8);
+
+        cpu_reference.memory[cpu_reference.HL] = A;
+        cpu_reference.HL++;
+        cpu_reference.PC++;
+    }
+
+    private void ld_hl_minus_a() {
+        System.out.println("_ld_hl_minus_a");
+
+        byte A = (byte) (cpu_reference.AF >> 8);
+
+        cpu_reference.memory[cpu_reference.HL] = A;
+        cpu_reference.HL--;
+        cpu_reference.PC++;
+    }
+
 }
--- src/main/java/Gameboy/CPUActions/Stack.java
+++ src/main/java/Gameboy/CPUActions/Stack.java
@@ -8,24 +8,24 @@ import java.util.Map;
 public class Stack extends CPUActions {
     private CPU cpu_reference;
 
-    private final Map<Character, Runnable> SUPPORTED_ACTIONS = new HashMap<>() {
-        {
-            put((char) 0x31, this::ld_sp_d16);
-        }
-
-        private void ld_sp_d16() {
-            System.out.println("_ld_sp_d16");
-            int lowerByte = cpu_reference.memory[cpu_reference.PC + 1] & 255;
-            int upperByte = cpu_reference.memory[cpu_reference.PC + 2] & 255;
-            cpu_reference.SP = (char) (upperByte << 8 | lowerByte);
-            cpu_reference.PC += 3;
-        }
-    };
+    private final Map<Character, Runnable> SUPPORTED_ACTIONS = Map.ofEntries(
+            Map.entry((char) 0x31, this::ld_sp_d16)
+    );
 
     // NOTE: This constructor is used by a dynamic factory and therefore intellij doesn't recognize its usage
     public Stack(CPU cpu) {
         cpu_reference = cpu;
     }
 
-    public Map<Character, Runnable> get_supported_actions() { return SUPPORTED_ACTIONS; }
+    public Map<Character, Runnable> get_supported_actions() {
+        return SUPPORTED_ACTIONS;
+    }
+
+    private void ld_sp_d16() {
+        System.out.println("_ld_sp_d16");
+        int lowerByte = cpu_reference.memory[cpu_reference.PC + 1] & 255;
+        int upperByte = cpu_reference.memory[cpu_reference.PC + 2] & 255;
+        cpu_reference.SP = (char) (upperByte << 8 | lowerByte);
+        cpu_reference.PC += 3;
+    }
 }
