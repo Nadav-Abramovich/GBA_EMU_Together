b428bdd9cf6dfb347396650f3d103ed0e4693907
nadav <nadav.abramovich@gmail.com>
2021-03-16 08:07:04 +0200

    Made sense of the rendering finally

--- src/main/java/gameboy/Screen/Screen.java
+++ src/main/java/gameboy/Screen/Screen.java
@@ -14,6 +14,7 @@ import java.nio.IntBuffer;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL14.glWindowPos2i;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
@@ -21,6 +22,8 @@ public class Screen {
     private final CPU cpu;
     // The window handle
     private final long window;
+    byte[][][] screen = new byte[256][256][4];
+    ByteBuffer screen_buffer = ByteBuffer.allocateDirect(256 * 256 * 4);
 
     public Screen(CPU cpu) {
         this.cpu = cpu;
@@ -131,38 +134,46 @@ public class Screen {
         }
     }
 
-    byte[] screen = new byte[(256 * 256) * 4];
-    ByteBuffer screen_buffer = ByteBuffer.allocateDirect(screen.length);
-
     private void putPixel(int x, int y, int line, int col, int color) {
-        int width = 32 * 8;
-        int real_x = x * 8 - col + 8;
-        byte vertical_y = cpu.memory.read_byte(0xFF42);
+        int width = 256;
 
-        int real_y = width - 30 - (y * 8 + line) + vertical_y - 100 + 16;
-        if (real_y > 0 && real_x > 0) {
-            screen[(real_x + real_y * width) * 4] = (byte) ((color >> 16) & 255);
-            screen[(real_x + real_y * width) * 4 + 1] = (byte) ((color >> 8) & 255);
-            screen[(real_x + real_y * width) * 4 + 2] = (byte) (color & 255);
-            screen[(real_x + real_y * width) * 4 + 3] = (byte) ((color >> 24) & 255);
+        int real_x = x*8 + (8-col);
 
+        int real_y = 256-(y*8 + line);
+        if (real_y > 0 && real_x > 0 && real_x < 256 && real_y < 256) {
+            screen[real_y][real_x][0] = (byte) ((color >> 16) & 255);
+            screen[real_y][real_x][1] = (byte) ((color >> 8) & 255);
+            screen[real_y][real_x][2] = (byte) (color & 255);
+            screen[real_y][real_x][3] = (byte) ((color >> 24) & 255);
+        }
+    }
+
+    private void putPixel2(int x, int y, int line, int col, int color) {
+        int width = 256;
+        int real_x = x + (8-col);
+
+        int real_y = 256 - (y + line);
+        if (real_y >= 0 && real_x >= 0 && real_x < 256 && real_y < 256) {
+            screen[real_y][real_x][0] = (byte) ((color >> 16) & 255);
+            screen[real_y][real_x][1] = (byte) ((color >> 8) & 255);
+            screen[real_y][real_x][2] = (byte) (color & 255);
+            screen[real_y][real_x][3] = (byte) ((color >> 24) & 255);
         }
     }
 
 
-    //TODO: LCD OAM DMA & cleanup
     private void draw_screen() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
         cpu.memory.write(0xFF44, (byte) (cpu.memory.read_byte(0xFF44) + 1));
 
         screen_buffer = ByteBuffer.allocateDirect(screen.length);
-        screen = new byte[(256 * 256) * 4];
+        screen = new byte[256][256][4];
         for (int x = 0; x < 256; x++) {
             for (int y = 0; y < 256; y++) {
-                screen[(y * 256 + x) * 4] = (byte) ((Color.WHITE.getRGB() >> 16) & 255);
-                screen[(y * 256 + x) * 4 + 1] = (byte) ((Color.WHITE.getRGB() >> 8) & 255);
-                screen[(y * 256 + x) * 4 + 2] = (byte) (Color.WHITE.getRGB() & 255);
-                screen[(y * 256 + x) * 4 + 3] = (byte) ((Color.WHITE.getRGB() >> 24) & 255);
+                screen[x][y][0] = (byte) ((Color.WHITE.getRGB() >> 16) & 255);
+                screen[x][y][1] = (byte) ((Color.WHITE.getRGB() >> 8) & 255);
+                screen[x][y][2] = (byte) (Color.WHITE.getRGB() & 255);
+                screen[x][y][3] = (byte) ((Color.WHITE.getRGB() >> 24) & 255);
             }
         }
         screen_buffer = ByteBuffer.allocateDirect(screen.length);
@@ -172,25 +183,23 @@ public class Screen {
             for (int x = 0; x < 32; x++) {
                 int val = cpu.memory.read_byte(0x9800 + y * 32 + x) & 255;
                 if (val != 0) {
-                    for (int spriteY = 0; spriteY < 8; spriteY++) {
-                        for (int spriteX = 0; spriteX < 8; spriteX++) {
-                            int sprite_pointer = 0x8000 + 0x10 * val;
+                    int sprite_pointer = 0x8000 + 0x10 * val;
 
-                            for (int line = 0; line < 8; line++) {
-                                for (int col = 0; col < 8; col++) {
-                                    int LOWER = (cpu.memory.read_byte(sprite_pointer + line * 2) & 255) & (1 << col);
-                                    int HIGHER = (cpu.memory.read_byte(sprite_pointer + line * 2 + 1) & 255) & (1 << col);
-                                    int COLOR = ((HIGHER << 1) | (LOWER)) >> col;
-                                    int color;
-                                    if (COLOR == 0) {
-                                        color = Color.WHITE.getRGB();
-                                    } else if (COLOR == 1) {
-                                        color = Color.LIGHT_GRAY.getRGB();
-                                    } else if (COLOR == 2) {
-                                        color = Color.DARK_GRAY.getRGB();
-                                    } else {
-                                        color = Color.BLACK.getRGB();
-                                    }
+                    for (int line = 0; line < 8; line++) {
+                        for (int col = 0; col < 8; col++) {
+                            int LOWER = (cpu.memory.read_byte(sprite_pointer + line * 2) & 255) & (1 << col);
+                            int HIGHER = (cpu.memory.read_byte(sprite_pointer + line * 2 + 1) & 255) & (1 << col);
+                            int COLOR = ((HIGHER << 1) | (LOWER)) >> col;
+                            int color;
+                            if (COLOR == 0) {
+                                color = Color.WHITE.getRGB();
+                            } else if (COLOR == 1) {
+                                color = Color.LIGHT_GRAY.getRGB();
+                            } else if (COLOR == 2) {
+                                color = Color.DARK_GRAY.getRGB();
+                            } else {
+                                color = Color.BLACK.getRGB();
+                            }
 //                                    int color = switch (COLOR) {
 //                                        case 0 -> Color.WHITE.getRGB();
 //                                        case 1 -> Color.BLACK.getRGB();
@@ -198,64 +207,20 @@ public class Screen {
 //                                        case 3 -> Color.RED.getRGB();
 //                                        default -> Color.BLUE.getRGB();
 //                                    };
-                                    putPixel(x, y, line, col, color);
-                                }
-                            }
+                            char vertical_y = (char) (cpu.memory.read_byte(0xFF42) & 255);
+                            putPixel(x, y, line - vertical_y, col, color);
                         }
                     }
                 }
             }
         }
-
+//
 
         // Window
         for (int y = 0; y < 32; y++) {
             for (int x = 0; x < 32; x++) {
                 byte val = cpu.memory.read_byte(0x9C00 + y * 32 + x);
                 if (val != 0) {
-                    for (int spriteY = 0; spriteY < 8; spriteY++) {
-                        for (int spriteX = 0; spriteX < 8; spriteX++) {
-                            var sprite_pointer = 0x8000 + 0x10 * val;
-
-                            for (int line = 0; line < 8; line++) {
-                                for (int col = 0; col < 8; col++) {
-                                    int LOWER = (cpu.memory.read_byte(sprite_pointer + line * 2) & 255) & (1 << col);
-                                    int HIGHER = (cpu.memory.read_byte(sprite_pointer + line * 2 + 1) & 255) & (1 << col);
-                                    int COLOR = ((HIGHER << 1) | (LOWER)) >> col;
-                                    int color;
-                                    if (COLOR == 0) {
-                                        color = Color.WHITE.getRGB();
-                                    } else if (COLOR == 1) {
-                                        color = Color.LIGHT_GRAY.getRGB();
-                                    } else if (COLOR == 2) {
-                                        color = Color.DARK_GRAY.getRGB();
-                                    } else {
-                                        color = Color.BLACK.getRGB();
-                                    }
-//                                    int color = switch (COLOR) {
-//                                        case 0 -> Color.WHITE.getRGB();
-//                                        case 1 -> Color.BLACK.getRGB();
-//                                        case 2 -> Color.GREEN.getRGB();
-//                                        case 3 -> Color.RED.getRGB();
-//                                        default -> Color.BLUE.getRGB();
-//                                    };
-                                    putPixel(x, y, line, col, color);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        for (int i = 0xFE00; i < 0xFE9F; i += 4) {
-            char y = (char) (cpu.memory.read_byte(i) & 255);
-            char x = (char) (cpu.memory.read_byte(i + 1) & 255);
-            char val = (char) (cpu.memory.read_byte(i + 2) & 255);
-            char attributes = (char) (cpu.memory.read_byte(i + 3) & 255);
-            for (int spriteY = 0; spriteY < 8; spriteY++) {
-                cpu.memory.write(0xFF44, (byte) (y * 8 + spriteY));
-                for (int spriteX = 0; spriteX < 8; spriteX++) {
                     var sprite_pointer = 0x8000 + 0x10 * val;
 
                     for (int line = 0; line < 8; line++) {
@@ -273,6 +238,13 @@ public class Screen {
                             } else {
                                 color = Color.BLACK.getRGB();
                             }
+//                                    int color = switch (COLOR) {
+//                                        case 0 -> Color.WHITE.getRGB();
+//                                        case 1 -> Color.BLACK.getRGB();
+//                                        case 2 -> Color.GREEN.getRGB();
+//                                        case 3 -> Color.RED.getRGB();
+//                                        default -> Color.BLUE.getRGB();
+//                                    };
                             putPixel(x, y, line, col, color);
                         }
                     }
@@ -280,8 +252,53 @@ public class Screen {
             }
         }
 
-        ByteBuffer pixels = screen_buffer.put(screen).flip();
+        for (char i = 0xFE00; i <= 0xFE9F; i += 4) {
+            char y = (char) ((cpu.memory.read_byte(i) & 255) - 16);
+            char x = (char) (cpu.memory.read_byte(i + 1) & 255 - 8);
+            char tile_index = (char) (cpu.memory.read_byte(i + 2) & 255);
+            char attributes = (char) (cpu.memory.read_byte(i + 3) & 255);
+            if (tile_index != 0) {
+                System.out.println("OKKKK");
+                System.out.println(Integer.toHexString(y).toUpperCase());
+                System.out.println(Integer.toHexString(x).toUpperCase());
+                System.out.println(Integer.toHexString((char) (cpu.memory.read_byte(i + 2) & 255)).toUpperCase());
+                System.out.println("OKKKK");
+            }
+            char sprite_pointer = (char) (0x8000 + 0x10 * tile_index);
+            for (int spriteY = 0; spriteY < 8; spriteY++) {
+                for (int spriteX = 0; spriteX < 8; spriteX++) {
+                    int LOWER = (cpu.memory.read_byte(sprite_pointer + spriteY * 2) & 255) & (1 << spriteX);
+                    int HIGHER = (cpu.memory.read_byte(sprite_pointer + spriteY * 2 + 1) & 255) & (1 << spriteX);
+                    int COLOR = ((HIGHER << 1) | (LOWER)) >> spriteX;
+                    int color;
+                    if (COLOR == 0) {
+                        color = Color.WHITE.getRGB();
+                    } else if (COLOR == 1) {
+                        color = Color.LIGHT_GRAY.getRGB();
+                    } else if (COLOR == 2) {
+                        color = Color.DARK_GRAY.getRGB();
+                    } else {
+                        System.out.println(COLOR);
+                        color = Color.BLACK.getRGB();
+                    }
+                    char vertical_y = (char) (cpu.memory.read_byte(0xFF42) & 255);
+                    putPixel2(x, y, spriteY, spriteX, color);
+                }
+            }
+        }
+        byte[] temp = new byte[256*256*4];
+        for (int y = 0; y < 255; y++) {
+            for (int x = 0; x < 255; x++) {
+                for (int z = 0; z < 4; z++) {
+                    temp[(y*256 + x) * 4 + z] = screen[y][x][z];
+                }
+            }
+        }
+        ByteBuffer screen_buffer = ByteBuffer.allocateDirect(temp.length);
+
+        ByteBuffer pixels = screen_buffer.put(temp).flip();
 //        GL11.glPixelZoom(1, 1);
+        glWindowPos2i(0, 0);
         GL11.glDrawPixels(256, 256, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, pixels);
 
         glfwSwapBuffers(window); // swap the color buffers
